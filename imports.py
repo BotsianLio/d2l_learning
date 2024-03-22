f1b117e386283cd0f78e39a94127cabd20207001
BotsianLio <116846325+BotsianLio@users.noreply.github.com>
2024-03-22 15:36:49 -0600

    Update imports.py

--- imports.py
+++ imports.py
@@ -5,6 +5,7 @@ from IPython import display
 from torch import nn
 from torch.utils import data
 from torchvision import transforms
+from torch.nn import functional as F
 from d2l import torch as d2l
 
 d2l.use_svg_display()
