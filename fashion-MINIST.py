c4f4c582be29c4b1dfcd702204e7b91ac1dab63e
BotsianLio <116846325+BotsianLio@users.noreply.github.com>
2024-03-22 13:57:13 -0600

    Create fashion-MINIST.py

---
+++ fashion-MINIST.py
@@ -0,0 +1,12 @@
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(
+    root="../data", train=True, transform=trans, download=True)
+mnist_test = torchvision.datasets.FashionMNIST(
+    root="../data", train=False, transform=trans, download=True)
+
+def get_fashion_mnist_labels(labels):  #@save
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+# return all labels
+
